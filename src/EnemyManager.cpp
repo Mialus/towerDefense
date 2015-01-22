b00dce01d3490c0d99091db991252127c81baa70
Erizino <temporos.biohazard@hotmail.fr>
2015-01-22 11:02:12 +0100

    Update EnemyManager.cpp
    Fixing issue for start identifier.

--- src/EnemyManager.cpp
+++ src/EnemyManager.cpp
@@ -8,7 +8,7 @@ namespace towerdefense {
     // Ajout des ennemies
     for(unsigned int i=0; i<level.size(); ++i){
       std::vector<MapIdentifier> line = level[i];
-      for(unsigned int j=0; j<level.size(); ++j){
+      for(unsigned int j=0; j<line.size(); ++j){
         if(line[j]==MapIdentifier::START){
           for(int nb=0; nb < nombreEnemy; nb++){
             allEnemy.push_back(Tank(i, j));
